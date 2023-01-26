6266a60311178e372c2bc2854a4375d26e035447
Mattheoo89 <matteobellomo.dev@gmail.com>
2023-01-26 15:16:09 +0100

    [add] milestone 2

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index(){
 
-        $projects = Project::all();
+        $projects = Project::with(['type', 'technologies'])->get();
 
         return response()->json(compact('projects'));
     }
