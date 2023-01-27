0b9709cecbd9c0fed27dd49f8d262648f258722d
Mattheoo89 <matteobellomo.dev@gmail.com>
2023-01-27 18:55:42 +0100

    fix fx show image

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -19,10 +19,10 @@ class ProjectController extends Controller
         
         $project = Project::where('slug', $slug)->with(['type', 'technologies'])->first();
 
-        if ($project->cover_image) {
+        if($project->cover_image){
             $project->cover_image = url("storage/" . $project->cover_image);
-        }else {
-            $project->cover_image = url('/storage/uploads/placeholder.jpg');
+        }else{
+            $project->cover_image = url("storage/uploads/image-paceholder.jpg");
         }
 
         return response()->json($project);
